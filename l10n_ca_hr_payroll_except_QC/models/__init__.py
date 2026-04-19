0b27dc781c675e4f51a0acadfaf44a358e1de12e
maplehornconsulting-art <maplehornconsulting@gmail.com>
2026-04-19 02:55:43 -0400

    Add files via upload

---
+++ l10n_ca_hr_payroll_except_QC/models/__init__.py
@@ -0,0 +1,10 @@
+# Part of MHC. See LICENSE file for full copyright and licensing details.
+
+from . import hr_employee
+from . import hr_version
+from . import hr_payslip
+from . import hr_payroll_t4
+from . import hr_payroll_t4_summary
+from . import hr_payroll_t4a
+from . import hr_payroll_t4a_summary
+from . import hr_payroll_roe

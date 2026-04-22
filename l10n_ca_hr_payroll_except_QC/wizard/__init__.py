0d01a07823ff3ed1aef2eb8b96828cb2536cd2e2
maplehornconsulting-art <maplehornconsulting@gmail.com>
2026-04-22 12:21:21 -0400

    Delete l10n_ca_hr_payroll_except_QC directory

--- l10n_ca_hr_payroll_except_QC/wizard/__init__.py
+++
@@ -1,5 +0,0 @@
-# Part of MHC. See LICENSE file for full copyright and licensing details.
-
-from . import hr_payroll_t4_wizard
-from . import hr_payroll_roe_wizard
-from . import l10n_ca_remittance_payment_wizard
